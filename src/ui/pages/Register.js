d36e3f687f7daa3484f30cf00c7227df065f06e2
cpepin <cam@cpepin.com>
2019-08-16 07:37:28 -0400

    Building out email validation

--- src/ui/pages/Register.js
+++ src/ui/pages/Register.js
@@ -4,6 +4,20 @@ import { Form } from 'informed';
 
 import TextField from '../components/TextField';
 import Main from '../layouts/main';
+import { isEmail } from '../utils/validation';
+
+const passwordHelpText = (
+  <>
+    Password must contain the following:
+    <ul>
+      <li>Must have two uppercase letters</li>
+      <li>One special character [!@#$&*]</li>
+      <li>Two digits</li>
+      <li>Three lowercase letters</li>
+      <li>A length of 8</li>
+    </ul>
+  </>
+);
 
 const Register = () => (
   <Main title="Sign up">
@@ -16,6 +30,8 @@ const Register = () => (
               field="registerEmail"
               name="email"
               label="Email"
+              validate={isEmail}
+              validateOnBlur
             />
 
             <TextField
@@ -24,6 +40,7 @@ const Register = () => (
               name="password"
               label="Password"
               type="password"
+              helpText={passwordHelpText}
             />
 
             <Button submit>Submit</Button>
--- src/ui/utils/validation.js
+++ src/ui/utils/validation.js
@@ -1,1 +1,5 @@
+import isEmailValid from 'validator/lib/isEmail';
+
 export const isRequired = value => !value ? 'Field is required.' : undefined;
+export const isEmail = value => !value || !isEmailValid(value) ? 'Please enter a valid email.' : undefined;
+

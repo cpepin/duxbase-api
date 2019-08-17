e1b61200ecbc0db537db97dadfb26a3ba29a11ac
cpepin <cam@cpepin.com>
2019-08-17 06:26:29 -0400

    Adding redirection to protected routes

--- src/ui/layouts/Protected.js
+++ src/ui/layouts/Protected.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Frame, AppProvider, TopBar } from '@shopify/polaris';
+import { useRouter } from 'next/router';
 
-const Protected = ({ children }) => (
-  <AppProvider>
-    <Frame topBar={<TopBar />}>
-      {children}
-    </Frame>
-  </AppProvider>
-);
+import useAuth from '../hooks/useAuth';
+import LoadingSkeleton from '../components/LoadingSkeleton';
+
+const Protected = ({ children }) => {
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/');
+    }
+  }, []);
+
+  return (
+    <AppProvider>
+      <Frame topBar={<TopBar />}>
+        {user ? children : <LoadingSkeleton />}
+      </Frame>
+    </AppProvider>
+  );
+};
 
 export default Protected;

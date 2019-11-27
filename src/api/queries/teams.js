474447ae33cb5666990456d1605898de7ad5690a
cpepin <cam@cpepin.com>
2019-11-27 08:38:45 -0500

    Updating /teams to return all played and managed teams for a user

--- src/api/queries/teams.js
+++ src/api/queries/teams.js
@@ -1,23 +1,26 @@
-const { executeQuery } = require('../utils/db');
+const { executeQuery } = require("../utils/db");
 
-function findTeams(params) {
+function findMemberAndManagedTeamsForUser(userId) {
   return executeQuery(_db =>
     _db
-      .select('team.*')
-      .from('team')
-      .where(params)
+      .select("team.*")
+      .from("team")
+      .where({ ["manager_id"]: userId })
+      .union([
+        _db
+          .select("team.*")
+          .from("team")
+          .leftJoin("player_team", "team.id", "player_team.team_id")
+          .where({ ["player_id"]: userId })
+      ])
   );
 }
 
 function createTeam(team) {
-  return executeQuery(_db => 
-    _db
-      .insert(team, ['*'])
-      .into('team')
-  );
+  return executeQuery(_db => _db.insert(team, ["*"]).into("team"));
 }
 
 module.exports = {
-  findTeams,
   createTeam,
+  findMemberAndManagedTeamsForUser
 };
--- src/api/routes/teams.js
+++ src/api/routes/teams.js
@@ -1,36 +1,48 @@
-const express = require('express');
-const Joi = require('joi');
-const boom = require('boom');
+const express = require("express");
+const Joi = require("joi");
+const boom = require("boom");
 
-const { createTeam, findTeams } = require('../queries/teams');
-const authenticated = require('../middleware/authenticated');
-const asyncMiddleware = require('../middleware/asyncMiddleware');
+const {
+  createTeam,
+  findMemberAndManagedTeamsForUser
+} = require("../queries/teams");
+const authenticated = require("../middleware/authenticated");
+const asyncMiddleware = require("../middleware/asyncMiddleware");
 
 const TeamsRouter = express.Router();
 
-TeamsRouter.get('/', authenticated, asyncMiddleware(async (req, res) => {
-  const teams = await findTeams({
-    ['team.user_id']: req.user.id,
-  });
+TeamsRouter.get(
+  "/",
+  authenticated,
+  asyncMiddleware(async (req, res) => {
+    const teams = await findMemberAndManagedTeamsForUser(req.user.id);
 
-  res.status(200).send(teams);
-}));
+    res.status(200).send(teams);
+  })
+);
 
-TeamsRouter.post('/', authenticated, asyncMiddleware(async (req, res) => {
-  const schema = {
-    name: Joi.string().required(),
-  };
+TeamsRouter.post(
+  "/",
+  authenticated,
+  asyncMiddleware(async (req, res) => {
+    const schema = {
+      name: Joi.string().required()
+    };
 
-  let newTeam = req.body;
+    let newTeam = req.body;
 
-  const { error } = Joi.validate(newTeam, schema, { abortEarly: false });
+    const { error } = Joi.validate(newTeam, schema, { abortEarly: false });
 
-  if (error) {
-    throw boom.badRequest('Invalid team', error);
-  }
+    if (error) {
+      throw boom.badRequest("Invalid team", error);
+    }
 
-  const [savedTeam] = await createTeam({ ...newTeam, ['user_id']: req.user.id });
-  return res.status(201).send(savedTeam);
-}));
+    const [savedTeam] = await createTeam({
+      ...newTeam,
+      ["user_id"]: req.user.id
+    });
+    return res.status(201).send(savedTeam);
+  })
+);
 
 module.exports = TeamsRouter;
